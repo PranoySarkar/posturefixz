1af04583f347df6571b4a2599d539ce0e9399a5c
Pranoy Sarkar <pranoy.sarkar@softwareag.com>
2020-01-04 00:49:31 +0530

    added more controlles

--- index.js
+++ index.js
@@ -15,45 +15,54 @@ window.addEventListener('load', _ => {
     let xCheckbox = document.querySelector('#xCheckbox');
     let yCheckbox = document.querySelector('#yCheckbox');
     let zCheckbox = document.querySelector('#zCheckbox');
+
+    let throttled = false;
     window.addEventListener('deviceorientation', (event) => {
 
-        if (positionLocked) {
-            op.innerHTML = '';
-            if (xCheckbox.checked && Math.abs(event.alpha - lockedAxis.x) > Number.parseFloat(delta.value)) {
-                op.innerHTML = `Out Of posture by x`
-                window.navigator.vibrate([50, 50, 50]);
-            }
-            if (yCheckbox.checked && Math.abs(event.beta - lockedAxis.y) > Number.parseFloat(delta.value)) {
-                op.innerHTML += `Out Of posture by Y`
-                window.navigator.vibrate([50, 20, 50]);
-            }
-            if (zCheckbox.checked && Math.abs(event.gamma - lockedAxis.z) > Number.parseFloat(delta.value)) {
-                op.innerHTML += `Out Of posture by Z`
-                window.navigator.vibrate([50, 80, 50]);
-            }
+        if (!throttled) {
+            throttled = true;
+            setTimeout((event) => {
+                if (positionLocked) {
+                    op.innerHTML = '';
+                    if (xCheckbox.checked && Math.abs(event.alpha - lockedAxis.x) > Number.parseFloat(delta.value)) {
+                        op.innerHTML = `Out Of posture by x`
+                        window.navigator.vibrate([200, 100, 200]);
+                    }
+                    if (yCheckbox.checked && Math.abs(event.beta - lockedAxis.y) > Number.parseFloat(delta.value)) {
+                        op.innerHTML += `Out Of posture by Y`
+                        window.navigator.vibrate([200, 100, 200]);
+                    }
+                    if (zCheckbox.checked && Math.abs(event.gamma - lockedAxis.z) > Number.parseFloat(delta.value)) {
+                        op.innerHTML += `Out Of posture by Z`
+                        window.navigator.vibrate([200, 100, 200]);
+                    }
 
+                }
+                else {
+                    axe = event.alpha;
+                    if (xCheckbox.checked) {
+                        xDom.innerHTML = `X ${axe}`;
+                    } else {
+                        xDom.innerHTML = ''
+                    }
+                    ye = event.beta;
+                    if (yCheckbox.checked) {
+                        yDom.innerHTML = `Y ${ye}`;
+                    }
+                    else {
+                        yDom.innerHTML = ''
+                    }
+                    zee = event.gamma;
+                    if (zCheckbox.checked) {
+                        zDom.innerHTML = `Z ${zee}`;
+                    } else {
+                        zDom.innerHTML = ''
+                    }
+                }
+                throttled = false;
+            }, 500, event)
         }
-        else {
-            axe = event.alpha;
-            if (xCheckbox.checked) {
-                xDom.innerHTML = `X ${axe}`;
-            }else{
-                xDom.innerHTML=''
-            }
-            ye = event.beta;
-            if (yCheckbox.checked) {
-                yDom.innerHTML = `Y ${ye}`;
-            }
-            else{
-                yDom.innerHTML=''
-            }
-            zee = event.gamma;
-            if (zCheckbox.checked) {
-                zDom.innerHTML = `Z ${zee}`;
-            }else{
-                zDom.innerHTML=''
-            }
-        }
+
     })
 
     document.querySelector('#lockBtn').addEventListener('click', _ => {
