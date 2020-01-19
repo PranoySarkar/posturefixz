3c857bb38a2f05281222b87fc6c1529873b28b6a
Pranoy Sarkar <pranoy.sarkar@softwareag.com>
2020-01-20 00:14:41 +0530

    normalized data

--- js/brain.js
+++ js/brain.js
@@ -7,37 +7,56 @@ var options = {
 };
 var chart = null;
 var data = [
-    ['time', 'mod-beta', 'pureBeta'],
+    ['time', 'beta', 'gamma'],
     [0, 0, 0]
 ];
 
-let index=0
+let index = 0
 /************************/
 let BETA = 0;
-let pureBeta=0;
+let GAMMA = 0
+let pureBeta = 0;
 let tempBeta
-let lastSomeBeta=[1,2,1,2,1,1,1,2,3,2,1,1,2,1,1,2,1,2,1,1,1,2,3,2];
-const temp={
-    lastBetaAvarage:0
+let lastSomeBeta =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+let pureGamma = 0;
+let tempGamma;
+let lastSomeGamma = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+const temp = {
+    lastUpdate: 0
 }
 
 
-window.addEventListener('deviceorientation', (event) => {
+window.addEventListener('deviceorientation', updatePosition);
+
+function updatePosition(event) {
+
+
+    tempGamma = Number.parseInt((event.gamma).toFixed(2));
+    pureGamma = tempGamma;
+    lastSomeGamma.shift();
+    lastSomeGamma.push(tempGamma)
+    let avg = lastSomeBeta.reduce((acc, snd) => acc + snd) / lastSomeGamma.length;
+    GAMMA = avg;
+
     tempBeta = Number.parseInt(Math.abs(event.beta - 90).toFixed(2));
-    pureBeta=tempBeta;
+    pureBeta = tempBeta;
     lastSomeBeta.shift();
     lastSomeBeta.push(tempBeta)
-    let avg = lastSomeBeta.reduce((acc,snd)=>acc+snd)/lastSomeBeta.length;
-    BETA=avg;
-    console.log(Math.abs(avg-tempBeta))
-    
-});
+    avg = lastSomeBeta.reduce((acc, snd) => acc + snd) / lastSomeBeta.length;
+    BETA = avg;
+}
 
 function checkDeviceOrientation() {
-    data.push([index++, BETA, tempBeta]);
+
+
+    data.push([index++, BETA, GAMMA]);
+    //data.push([index++, GAMMA, tempGamma]);
     update()
 }
-setInterval(checkDeviceOrientation, 200);
+setInterval(checkDeviceOrientation, 500);
 /*****************************************/
 
 
@@ -64,7 +83,7 @@ function clean() {
     leanFbData.innerHTML = '';
     tiltLrData.innerHTML = '';
     data = [
-        ['time', 'mod-beta', 'pure beta'],
+        ['time', 'beta', 'gamma'],
         [0, 0, 0]
     ];
     update();
